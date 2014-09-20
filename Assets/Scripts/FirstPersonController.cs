eb9443489845dc035ae39eeda57e72f11184fd10
ecsit <ecsit@localhost>
2014-09-20 03:29:13 -0400

    game master works through player

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -4,43 +4,47 @@ using System;
 
 
 public class FirstPersonController : MonoBehaviour {
-	
-	private float movementSpeed = 6.001f; 
-	private float mouseSensetivity = 10.0f;
-	//private float rotYSpeed = 1.01f;
-	private float upDownRange = 70.0f;
-	private float rotUpDown = 0;
-	private float verticalSpeed = 0.0f;
-	private float jumpHeight = 10.0f;
-	Vector3 speed;
 
 
+	private GameMaster GM;
+
+	private float rotUpDown;// = 0;
+	private Vector3 speed;
+	private float verticalSpeed;
+	private float rotLeftRight;
 
 	CharacterController characterController;
 	// Use this for initialization
 	void Start () {
+
+		GM  = GameObject.Find("Game Master").GetComponent<GameMaster>();
+
+		rotLeftRight = 0.0f; 
 		speed = Vector3.zero;
 		Screen.lockCursor = true;
 		characterController = GetComponent<CharacterController>();
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
 		//player rotation
 		//left and right
-		float rotLeftRight = Input.GetAxis("Mouse X")*mouseSensetivity;
+
+		rotLeftRight = Input.GetAxis("Mouse X")*GM.mouseSensetivity;
 		transform.Rotate(0, rotLeftRight, 0);
 		//up and down (with camera)
-		rotUpDown -= Input.GetAxis("Mouse Y")*mouseSensetivity;
-		rotUpDown = Mathf.Clamp(rotUpDown, -upDownRange, upDownRange);
+		rotUpDown -= Input.GetAxis("Mouse Y")*GM.mouseSensetivity;
+		rotUpDown = Mathf.Clamp(rotUpDown, -GM.upDownRange, GM.upDownRange);
 		
-		Camera.main.transform.localRotation = Quaternion.Euler( ,0,0);
+		Camera.main.transform.localRotation = Quaternion.Euler(rotUpDown,0,0);
 
 				
 		//Movement
 		//Jumping!!
 		if(characterController.isGrounded && Input.GetButtonDown("Jump")){
-			verticalSpeed = jumpHeight;
+			verticalSpeed = GM.jumpHeight;
 			Debug.Log("Jumping attempted!");
 		}
 		else if(Input.GetButtonDown("Jump")){
@@ -54,17 +58,17 @@ public class FirstPersonController : MonoBehaviour {
 		}
 		//Running!!
 		if(characterController.isGrounded && Input.GetKeyDown(KeyCode.LeftShift)){
-			movementSpeed = 10.0f;
+			GM.movementSpeed = 10.0f;
 		}
-		else if(movementSpeed == 10.0f){
-			movementSpeed = 6.0f;
+		else if(GM.movementSpeed == 10.0f){
+			GM.movementSpeed = 6.0f;
 		}
 		
 		
 		float forwardSpeed = Input.GetAxis("Vertical");
 		float sideSpeed = Input.GetAxis("Horizontal");
 		
-		speed = new Vector3( sideSpeed*movementSpeed, verticalSpeed, forwardSpeed*movementSpeed);
+		speed = new Vector3( sideSpeed*GM.movementSpeed, verticalSpeed, forwardSpeed*GM.movementSpeed);
 		
 		speed = transform.rotation * speed;
 		
