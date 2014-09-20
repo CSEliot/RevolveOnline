9540d5e9d48db7a146ef91ff4750c71a115a223c
ecsit <ecsit39@gmail.com>
2014-09-20 03:29:13 -0400

    game master works through player

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+
+	//###################################
+	//PLAYER VARIABLES
+	//###################################
+	public float movementSpeed; 
+	public float mouseSensetivity;// = 10.0f;
+	public float upDownRange;// = 70.0f;
+	public float jumpHeight;// = 10.0f;
+
+	//###################################
+	//ENVIRONMENT VARIABLES
+	//###################################
+
+
+
+	//###################################
+	//MOD CONTENT ENABLED
+	//###################################
+	//GUNS/WEAPONS-----------------------
+
+
+
+
+	//Characters-------------------------
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
