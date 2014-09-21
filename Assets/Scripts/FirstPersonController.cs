af7c3cbd552c78c394390a989d7fac15f92c18e3
eliot2 <ecsit39@gmail.com>
2014-09-21 16:50:09 -0400

    DEAR LORD JESUS

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -13,9 +13,23 @@ public class FirstPersonController : MonoBehaviour {
 	private float verticalSpeed;
 	private float rotLeftRight;
 
+
+	//ACTION STRINGS
+	//==================================================================
+	private string Haim_str = "_Look Rotation";
+	private string Vaim_str = "_Look UpDown";
+	private string Strf_str = "_Strafe";
+	private string FWmv_str = "_Forward";
+	public string  Fire_str = "_Fire";
+	private string Jump_str = "_Jump";
+
+
+
 	CharacterController characterController;
 	// Use this for initialization
 	void Start () {
+
+		setControlStrings();
 
 		GM  = GameObject.Find("Game Master").GetComponent<GameMaster>();
 
@@ -32,46 +46,67 @@ public class FirstPersonController : MonoBehaviour {
 		//player rotation
 		//left and right
 
-		rotLeftRight = Input.GetAxis("Mouse X")*GM.mouseSensetivity;
+		rotLeftRight = Input.GetAxis(Haim_str)*GM._M.mouseSensetivity;
 		transform.Rotate(0, rotLeftRight, 0);
 		//up and down (with camera)
-		rotUpDown -= Input.GetAxis("Mouse Y")*GM.mouseSensetivity;
-		rotUpDown = Mathf.Clamp(rotUpDown, -GM.upDownRange, GM.upDownRange);
+		rotUpDown -= Input.GetAxis(Vaim_str)*GM._M.mouseSensetivity;
+		rotUpDown = Mathf.Clamp(rotUpDown, -GM._M.upDownRange, GM._M.upDownRange);
 		
-		Camera.main.transform.localRotation = Quaternion.Euler(rotUpDown,0,0);
+		transform.GetChild(0).transform.localRotation = Quaternion.Euler(rotUpDown,0,0);
 
 				
 		//Movement
 		//Jumping!!
-		if(characterController.isGrounded && Input.GetButtonDown("Jump")){
-			verticalSpeed = GM.jumpHeight;
+		if(characterController.isGrounded && Input.GetButtonDown(Jump_str)){
+			verticalSpeed = GM._M.jumpHeight;
 			Debug.Log("Jumping attempted!");
 		}
-		else if(Input.GetButtonDown("Jump")){
+		else if(Input.GetButtonDown(Jump_str)){
 			Debug.Log("Jumping attempted! and FAILED");
 		}
 		if(characterController.isGrounded && verticalSpeed < 0){
 			verticalSpeed = 0.0f;
 		}
 		else{
-			verticalSpeed += Physics.gravity.y * Time.deltaTime;
+			verticalSpeed += GM._M.gravity * Time.deltaTime;
 		}
 		//Running!!
 		if(characterController.isGrounded && Input.GetKeyDown(KeyCode.LeftShift)){
-			GM.movementSpeed = 10.0f;
+			GM._M.movementSpeed = 10.0f;
 		}
-		else if(GM.movementSpeed == 10.0f){
-			GM.movementSpeed = 6.0f;
+		else if(GM._M.movementSpeed == 10.0f){
+			GM._M.movementSpeed = 6.0f;
 		}
 		
 		
-		float forwardSpeed = Input.GetAxis("Vertical");
-		float sideSpeed = Input.GetAxis("Horizontal");
+		float forwardSpeed = Input.GetAxis(FWmv_str);
+		float sideSpeed = Input.GetAxis(Strf_str);
 		
-		speed = new Vector3( sideSpeed*GM.movementSpeed, verticalSpeed, forwardSpeed*GM.movementSpeed);
+		speed = new Vector3( sideSpeed*GM._M.movementSpeed, verticalSpeed, forwardSpeed*GM._M.movementSpeed);
 		
 		speed = transform.rotation * speed;
 		
 		characterController.Move( speed * Time.deltaTime );
 	}
+
+	private void setControlStrings(){
+		string pName = gameObject.name;
+
+		if(pName == "Player 1"){
+			Fire_str = "p1" + Fire_str;
+			FWmv_str = "p1" + FWmv_str;
+			Strf_str = "p1" + Strf_str;
+			Haim_str = "p1" + Haim_str;
+			Vaim_str = "p1" + Vaim_str;
+			Jump_str = "p1" + Jump_str;
+		}else if(pName == "Player 2"){
+			Fire_str = "p2" + Fire_str;
+			FWmv_str = "p2" + FWmv_str;
+			Strf_str = "p2" + Strf_str;
+			Haim_str = "p2" + Haim_str;
+			Vaim_str = "p2" + Vaim_str;
+			Jump_str = "p2" + Jump_str;
+		}
+	}
+
 }
--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,43 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+
 
 public class GameMaster : MonoBehaviour {
 
+	[Serializable]
+	public struct GAME_VALUES{
 
-	//###################################
-	//PLAYER VARIABLES
-	//###################################
-	public float movementSpeed; 
-	public float mouseSensetivity;// = 10.0f;
-	public float upDownRange;// = 70.0f;
-	public float jumpHeight;// = 10.0f;
+		//###################################
+		//PLAYER VARIABLES
+		//###################################
+		public float movementSpeed; 
+		public float mouseSensetivity;// = 10.0f;
+		public float upDownRange;// = 70.0f;
+		public float jumpHeight;// = 10.0f;
 
-	//###################################
-	//ENVIRONMENT VARIABLES
-	//###################################
+		public float maxHealth;
 
+		//###################################
+		//ENVIRONMENT VARIABLES
+		//###################################
+		public float gravity;
+		public bool jumpingAllowed;
 
-
-	//###################################
-	//MOD CONTENT ENABLED
-	//###################################
-	//GUNS/WEAPONS-----------------------
-
-
-
-
-	//Characters-------------------------
-
-
+		//###################################
+		//MOD CONTENT ENABLED
+		//###################################
+		//GUNS/WEAPONS-----------------------
+		//basic gun
+		public float bulletSpeed_Basic;
+		public float gunWeight;
 
 
+		//Characters-------------------------
 
-	// Use this for initialization
-	void Start () {
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public GAME_VALUES _M;
+
+	void Start(){
+		Save_Values();
+		Load_Values();
+	}
+
+	void Update(){
+		if(Input.GetKey("escape")){
+			Save_Values();
+			Manager.say("Attempting to quit game now, goodbye!");
+			Application.Quit();
+
+		}
+	}
+
+
+	private void Save_Values(){
+		GameMaster masterScript;
+		masterScript = GameObject.Find("Game Master").GetComponent<GameMaster>();
+		Manager.say("Attempting to save GM");
+		IFormatter formatter = new BinaryFormatter();
+		Stream stream = new FileStream("GM.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+		formatter.Serialize(stream, masterScript._M);
+		stream.Close();
+		Manager.say("Saving GM likely successful");
+	}
+
+	private void Load_Values(){
+		IFormatter formatter = new BinaryFormatter();
+		Stream stream = new FileStream("GM.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+		_M = (GAME_VALUES) formatter.Deserialize(stream);
+		stream.Close();
+
 	}
 }
