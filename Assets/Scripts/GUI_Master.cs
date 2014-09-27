49ed44c390d1720661725e8726a90067a459f966
ecsit <ecsit39@gmail.com>
2014-09-27 15:06:18 -0400

    Added tons of art, reorganized files and renamed more things

---
+++ Assets/Scripts/GUI_Master.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_Master : MonoBehaviour {
+
+	public GUIStyle varia;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUILayout.Box("Game Variables", varia);
+	}
+}
