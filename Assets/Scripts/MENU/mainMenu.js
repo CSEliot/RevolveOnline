8017a3f242ab2d01b57fa716d18e6e0152ce2084
Graham Dolle <darkmastergraham@gmail.com>
2014-09-30 13:42:14 -0400

    Added main menu..minimal

---
+++ Assets/Scripts/MENU/mainMenu.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var style : GUIStyle;
+var style2 : GUIStyle;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI()
+{
+	if(GUI.Button(Rect(Screen.width/2-50, Screen.height/2+100, 100, 100), "Play", style2))
+	{
+		Application.LoadLevel(Application.loadedLevel+1);
+	}
+	
+	style.normal.textColor.a = 1;
+	GUI.Label(Rect(Screen.width/2-50, Screen.height/2-50, 100, 100), "Revolve Online", style);
+	style.normal.textColor.a = .3;
+}
