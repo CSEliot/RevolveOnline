94223e277ee24ce10719f9acad32a9cae481f59c
Larry Prince <graham.p.sullivan@gmail.com>
2014-10-15 12:26:57 -0400

    added shotgun

--- Assets/Scripts/Weapons/Gun_LAZAR.cs
+++ Assets/Scripts/Weapons/Gun_LAZAR.cs
@@ -1,30 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gun_Basic : MonoBehaviour {
-
+public class Gun_LAZAR : MonoBehaviour {
+	
 	
 	//private Vector3 startRotation = new Vector3(0f,0f,0f);
 	//private Vector3 startScale = new Vector3(0.17989f, .13f, 0.56067f);
-
+	
 	private bool equipped;
-    public bool spawnedEquipped;
+	public bool spawnedEquipped;
 	private string Fire_str = " "; 
-
+	
 	private float fireSpd;
-
+	
 	public GameObject bullet_prefab;
-
+	
 	private GameMaster GM;
-
+	
 	private string owner;
-
+	
 	// Use this for initialization
 	void Start () {
 		fireSpd = 0;
-
+		
 		GM  = GameObject.Find("Game Master").GetComponent<GameMaster>();
-
+		
 		if( !spawnedEquipped){
 			equipped = false;
 		}else{
@@ -37,43 +37,50 @@ public class Gun_Basic : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		fireSpd -= Time.deltaTime*60;
-
+		
 		Quaternion tempRot = gameObject.transform.rotation;
 		if(equipped && Input.GetButton(Fire_str) && fireSpd < 0){
 			Manager.say("I FIRED", "eliot");
 			GameObject tempBullet;
-            //the gun has a bullet spawn component found via getchild(0).transform.position 
-			tempBullet = Instantiate(bullet_prefab, transform.GetChild(0).transform.position,  tempRot*Quaternion.Euler(new Vector3(90f,0f,0f))) as GameObject;
-			tempBullet.GetComponent<Bullet>().setSpeedandOwner(Vector3.up * (GM._M.bulletSpeed_Basic*10)*(GM._M.bulletSpeed_Basic*10), owner);
-			fireSpd = GM._M.fireInterval_Basic;
+			//the gun has a bullet spawn component found via getchild(0).transform.position 
+
+			Vector3 offset = Vector3.zero;
+			for(int i=0; i < 20; i++)
+			{
+				tempBullet = Instantiate(bullet_prefab, transform.GetChild(0).transform.position+offset,  tempRot*Quaternion.Euler(new Vector3(90f,0f,0f))) as GameObject;
+				tempBullet.GetComponent<Bullet>().setSpeedandOwner(Vector3.up * (GM._M.bulletSpeed_Basic*7)*(GM._M.bulletSpeed_Basic*8), owner);
+				fireSpd = GM._M.fireInterval_Basic;
+				offset += new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), 0);
+			}
 		}
 	}
-
-
+	
+	
 	//METHOD USED IF PLAYER SPAWNS WITH THIS GUN
 	private void setEquips(GameObject player){
 		if(!equipped && player.transform.tag == "Player" && !player.GetComponent<FirstPersonController>().gunEquipped){
-            player.GetComponent<FirstPersonController>().gunEquipped = true;
-            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-            player.GetComponent<GunSetup>().attachGun(gameObject, player);
-            equipped = true;
-            //transform.localScale.Set(startScale.x, startScale.y, startScale.z);
-            Fire_str = transform.GetComponentInParent<FirstPersonController>().GetFire_Str();
-            owner = transform.parent.parent.name;
+			player.GetComponent<FirstPersonController>().gunEquipped = true;
+			transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+			player.GetComponent<GunSetup>().attachGun(gameObject, player);
+			equipped = true;
+			//transform.localScale.Set(startScale.x, startScale.y, startScale.z);
+			Fire_str = transform.GetComponentInParent<FirstPersonController>().GetFire_Str();
+			owner = transform.parent.parent.name;
 		}
 	}
-
+	
 	//METHOD USED IF NO SPAWNING WITH GUN
 	void OnTriggerEnter(Collider player){
 		if(!equipped && player.transform.tag == "Player" && !player.GetComponent<FirstPersonController>().gunEquipped){
 			player.GetComponent<FirstPersonController>().gunEquipped = true;
-            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-            player.GetComponent<GunSetup>().attachGun(gameObject, player.gameObject);
+			transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+			player.GetComponent<GunSetup>().attachGun(gameObject, player.gameObject);
 			equipped = true;
 			//transform.localScale.Set(startScale.x, startScale.y, startScale.z);
-            Fire_str = transform.GetComponentInParent<FirstPersonController>().GetFire_Str();
+			Fire_str = transform.GetComponentInParent<FirstPersonController>().GetFire_Str();
 			owner = transform.parent.parent.name;
 		}
 	}
 }
+
 
