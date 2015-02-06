144bc0c277d6afa080e4f0bdb06470beb8d1d257
Conrad <conradte3@gmail.com>
2015-02-06 14:34:52 -0500

    Moar meanew

---
+++ Assets/Scripts/MENU/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    public GameObject playPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void PlayOptions()
+    {
+        playPanel.SetActive(true);
+    }
+}
