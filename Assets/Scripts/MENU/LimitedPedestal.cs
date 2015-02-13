def352371c5a53877e58c150840a5886ab518810
Conrad <conradte3@gmail.com>
2015-02-13 15:57:24 -0500

    Some map selection stuff

---
+++ Assets/Scripts/MENU/LimitedPedestal.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitedPedestal : MonoBehaviour {
+
+    private float currentRotation;
+    public float rotationSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentRotation = 0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if (transform.rotation.x > 0.95 || transform.rotation.x > -0.95)
+        {
+            rotationSpeed *= -1;
+        }
+
+        currentRotation = ((rotationSpeed));
+        Debug.Log(transform.rotation.x);
+
+        //Manager.say("Current Rotation: " + currentRotation + "Actual Rotation: " + transform.rotation.y);
+
+        //transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, currentRotation, this.transform.eulerAngles.z);
+        transform.Rotate(0, currentRotation, 0);
+        //if (transform.childCount == 0)
+        //{
+        //    Manager.say("Pedestal Destroyed!", "eliot");
+        //    Destroy(gameObject);
+        //}
+    }
+}
--- Assets/Scripts/MENU/MenuManager.cs
+++ Assets/Scripts/MENU/MenuManager.cs
@@ -3,10 +3,23 @@ using System.Collections;
 
 public class MenuManager : MonoBehaviour {
     public GameObject playPanel;
+    public GameObject mainMenu;
+    public GameObject charSelect;
+    public GameObject mapSelect;
+    public GameObject load;
+
+    public GameObject[] charModels;
+    public GameObject[] mapModels;
+    //public GameObject[] selectedChars = new GameObject[4];
+
+
+    public int currentChar = 0;
+    public int currentMap = 0;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        
 	}
 	
 	// Update is called once per frame
@@ -18,5 +31,100 @@ public class MenuManager : MonoBehaviour {
     public void PlayOptions()
     {
         playPanel.SetActive(true);
+    }
+
+    //switches to or from the main menu
+    public void SwitchMainMenu()
+    {
+        if (mainMenu.activeSelf)
+        {
+            mainMenu.gameObject.SetActive(false);
+        }
+        else if (mainMenu.activeSelf == false)
+        {
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+
+
+    //switches to or from the character selection menu
+    public void SwitchCharacterSelection()
+    {
+        //Enable or disable the character selection panel
+        if (charSelect.activeSelf)
+        {
+            charSelect.gameObject.SetActive(false);
+        }
+        else if (charSelect.activeSelf == false)
+        {
+            charSelect.gameObject.SetActive(true);
+        }
+
+        //Enable or disable the rotating character
+        if (charModels[currentChar].activeSelf)
+        {
+            charModels[currentChar].gameObject.SetActive(false);
+        }
+        else if (charModels[currentChar].activeSelf == false)
+        {
+            charModels[currentChar].gameObject.SetActive(true);
+        }
+    }
+
+
+
+    //switches to or from the map selection menu
+    public void SwitchMapSelection()
+    {
+        if (mapSelect.activeSelf)
+        {
+            mapSelect.gameObject.SetActive(false);
+        }
+        else if (mapSelect.activeSelf == false)
+        {
+            mapSelect.gameObject.SetActive(true);
+        }
+    }
+
+    public void LoadButton()
+    {
+        load.SetActive(true);
+    }
+
+
+
+    //Selects the next or previous character as the character for that player to start the game with
+    public void changeChar(int direction)
+    {
+        GameObject[] playerList = GameObject.FindGameObjectsWithTag("Player");
+        
+        //Enable disable the current character model and enable the new one
+        if (currentChar + direction >= 0 && currentChar + direction < charModels.Length)
+        {
+            charModels[currentChar].SetActive(false);
+            currentChar += direction;
+            charModels[currentChar].SetActive(true);
+            //selectedChars[0] = playerList[currentChar];
+        }
+    }
+
+    //public void PlayGame()
+    //{
+
+    //}
+
+    //Selects the next or previous map as the map to start the game with
+    public void changeMap(int direction)
+    {
+        //disable current map model and enable next
+        if (currentMap + direction >= 0 && currentMap + direction < mapModels.Length)
+        {
+            mapModels[currentMap].SetActive(false);
+            currentMap += direction;
+            mapModels[currentMap].SetActive(true);
+            //selectedChars[0] = playerList[currentChar];
+        }
+
+        //set start map to new map
     }
 }
