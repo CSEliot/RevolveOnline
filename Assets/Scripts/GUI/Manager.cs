f2f1e3083009a03b299e4db2dc92b54d79051a75
ecsit <ecsit@localhost>
2015-07-17 03:45:14 -0400

    Updated GFX, added jumping while in air and fixed huebot gun position. TBD: Huebot gun aim straighter and huebot move like huebot

--- Assets/Scripts/GUI/Manager.cs
+++ Assets/Scripts/GUI/Manager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public static class Manager {
 
 	public const bool DEBUG = true;
-	private const string myName = "always";
+	private const string myName = "Eli";
 	//change to your name above, then whenever you want to see your own errors, call the function with your name.
 
 
